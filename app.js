abdc6e4465a0335cf3f1a1517469cf21e9ae9b6f
Abhi3207 <79158225+Abhi3207@users.noreply.github.com>
2021-06-19 23:55:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const request = require('request')
+const app = express()
+
+
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended: true}));
+app.set('view engine', 'ejs')
+
+let apikey='4901e85860bb94e57b1ac15062076c81';
+ 
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+
+app.post('/', (req, res)=> {
+    let city = req.body.city;
+    const url=`http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`; 
+
+  request(url,(err, response, body)=> {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weather = JSON.parse(body)
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `Temp= ${weather.main.temp}, location=${weather.name},  wind speed=${weather.wind.speed}, humidity=${weather.main.humidity}, pressure=${weather.main.pressure}`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+      }
+    });
+  })
+app.get('*', (req,res)=>{
+    console.log('data not found')
+})
+
+app.listen(5000,() => {
+    console.log('Example app listening on port 3000!')
+  })
